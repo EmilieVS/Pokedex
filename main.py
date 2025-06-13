aa028c6d01a7eff91c3b4c2c48888bf4a047d606
Emilie VINCENT-SULLY <emivsy@gmail.com>
2025-06-13 15:43:08 +0200

    first steps done, corrected and commented

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
 import json
 
+# Creating function to get the file I imported previously and parse it. 
+
 def readJSON():
     try:
-        with open('pokedex.json', 'r', encoding='utf-8') as file:
-            data = json.load(file)
-            print(data)
+        with open('pokedex.json', 'r') as jsonFILE: #Reading the file
+            data = json.load(jsonFILE) # Parsing the file
+            print(json.dumps(data, indent=4)) # indenting so we have a clear object-like result 
+            
+            
         
     except FileNotFoundError:
         print('Sorry, file not found')
+
     
 
+
+# Test: working ok
 readJSON()
+
+
+
+
 
 
         
