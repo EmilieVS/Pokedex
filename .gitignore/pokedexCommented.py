63b361f0c8cb6cc648153728eef15e055a00406c
Emilie VINCENT-SULLY <emivsy@gmail.com>
2025-06-14 18:02:55 +0200

    step 3 done

--- .gitignore/pokedexCommented.py
+++ .gitignore/pokedexCommented.py
@@ -1,11 +1,13 @@
 import json
 
+#This is a fully commented version of the code for learning or futur improvement(if I have to get back to it when practicing) 
+
 # Creating function to get the file I imported previously and parse it. 
 def readJSON():
     try:
-        with open('pokedex.json', 'r') as jsonFILE: 
-            data = json.load(jsonFILE) 
-            
+        with open('pokedex.json', 'r') as jsonFILE: #Reading the file
+            data = json.load(jsonFILE) # Parsing the file
+            #print(json.dumps(data, indent=4)) # indenting so we have a clear object-like result 
             return data
     
     except FileNotFoundError:
@@ -15,9 +17,11 @@ pokeData = readJSON()
 
 # function to count the number of Pokemon
 def countPokemon():
-            numOfPokemon = len(pokeData['pokemon']) 
-            print(f"Il y a {numOfPokemon} Pokémons dans le Pokédex")
+
+            numOfPokemon = len(pokeData['pokemon']) # len function to get the number of element
+            print(f"Il ya {numOfPokemon} Pokémons dans le Pokédex")
             
+#countPokemon()
 
 #Exercice wants me to do the same function but using a loop
 def countLoopPokemon():
@@ -27,6 +31,8 @@ def countLoopPokemon():
                
             print(f"Il y a {count} Pokémons")
 
+
+#countLoopPokemon()
 
 # Function to count Pokemons over 10kg
 def pokemonsWeight():
@@ -41,12 +47,34 @@ def pokemonsWeight():
 
     print(f"Il y a {count} Pokémons qui pèsent plus de 10 kg")
 
+
+#pokemonsWeight()
+
+
 #Function to sort them by weight
 def sortPokemon():
+#getting the list sorted : "key" works with functions only so using lambda(anonymous function)
     pokeData['pokemon'].sort(key = lambda pokemon:float(pokemon['weight'].replace("kg",'')))
       
     for pokemon in pokeData['pokemon']:
          print(f"{pokemon['name']} : {pokemon['weight']}")
+        
+
+#sortPokemon()
+#This function is useless as it works only for the first pokemon. Have ot think it through...
+# def nextEvolution(pokemon):
+#           for pokemon in pokeData['pokemon']:
+#             #pokemon = pokeData['pokemon'][0]['name']
+#             pokemon = pokemon['name']
+#             evolution = pokeData['pokemon'][0]['next_evolution']
+#           print(pokemon)
+
+# nextEvolution('Charmander')
+
+
+#Last thing is to get a pokemon evolution based on his name. 
+#With 151 of them in the list, it is better to make use of classes ! Let's ma ke a Pokemon class based on the data inside the JSON so I can make pokemon for each one in the file
+#Let's make a Pokemon class that will return the data I want. 
 
 class Pokemon:
      def __init__(self,pokeData):
@@ -72,13 +100,13 @@ class Pokemon:
         name = []
         for evolution in self.next_evolution:
              name.append(evolution['name'])
-        return " puis ".join(name)
+        return ", ".join(name)
       else : 
            return None
 
 
 
-#On instancie un objet pokemon pour chaque entrée du JSON
+#Now creating the pokemon and getting only the data I want (evolution)
 def pokedex():
      for pokemon in pokeData['pokemon']:
           newPokemon = Pokemon(pokemon)
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Pokedex
 
 This is an exercice for my school. 
-I realise I can now manage 3 languages but I've showed nothing in Python so far on my GitHub. 
+I realised I can now code in 3 languages but I've showed nothing in Python so far on my GitHub. 
 This is the language I've started programming with and I'm a Pokemon fan.
 
 So let's have fun and catch them all (with Python) !
